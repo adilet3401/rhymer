68df2fcdc39be392332e83a74ccbeaf8df23df35
adilet <adilet.dildeev08@gmail.com>
2025-05-18 00:07:06 +0600

    в

---
+++ lib/router/routes.dart
@@ -0,0 +1,12 @@
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+
+ @override
+ List<AutoRoute> get routes => [
+   /// routes go here
+ ];
+}
+
+class AutoRouterConfig {
+  const AutoRouterConfig();
+}
