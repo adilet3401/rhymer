0d10d36da8d5b709509397e708f21e33124373a9
adilet <adilet.dildeev08@gmail.com>
2025-05-18 19:38:17 +0600

    настроил rhymerapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:rhymer/router/router.dart';
 import 'features/home/view/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RhymerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RhymerApp extends StatefulWidget {
+  const RhymerApp({super.key});
+
+  @override
+  State<RhymerApp> createState() => _RhymerAppState();
+}
+
+class _RhymerAppState extends State<RhymerApp> {
+  final _router = AppRouter(); //autoroute
 
   @override
   Widget build(BuildContext context) {
     final primaryColor = Color(0xffF82B10);
-    return MaterialApp(
+    return MaterialApp.router(
+      //autoroute
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -19,7 +28,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Color(0xffeff1f3),
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
       ),
-      home: HomeScreen(),
+      routerConfig: _router.config(),
     );
   }
 }
