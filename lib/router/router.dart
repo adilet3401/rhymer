9bc593c44f545c345b491736937265be55f7710c
adilet <adilet.dildeev08@gmail.com>
2025-05-18 19:34:53 +0600

    написал все пути с помощью авто роут

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,10 +1,14 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:rhymer/router/router.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
-
- @override
- List<AutoRoute> get routes => [
-   /// routes go here
- ];
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(page: HomeRoute.page, path: '/'),
+    AutoRoute(page: FavoritesRoute.page, path: '/favorites'),
+    AutoRoute(page: SearchRoute.page, path: '/search'),
+    AutoRoute(page: PoemsRoute.page, path: '/poems'),
+    AutoRoute(page: SettingRoute.page, path: '/settings'),
+  ];
 }
